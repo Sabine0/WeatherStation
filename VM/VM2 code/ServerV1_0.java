1c336a420b7be0d2554e8879a21ed4ad602b096d
danial <56028352+danialB06@users.noreply.github.com>
2021-01-25 21:52:37 +0100

    changes

--- VM/VM2 code/ServerV1_0.java
+++ VM/VM2 code/ServerV1_0.java
@@ -2,31 +2,34 @@ import java.io.*;
 import java.net.*;
 
 /**
- * deze klasse luistert naar een poort en als er een klient is gaat die de data ontvangen. 
+ * deze klasse luistert naar een poort en als er een klient is gaat die de data
+ * ontvangen.
  *
  */
 
 public class ServerV1_0 {
     public static void main(String[] args) {
-        
-            try {
-                // creating the server
-                System.out.println("server started");
-                ServerSocket ss = new ServerSocket(7789);
-                System.out.println("server is waiting for client reqeut");
-                while(true){
+
+        try {
+            // creating the server
+            System.out.println("server started");
+            ServerSocket ss = new ServerSocket(7789);
+            System.out.println("server is waiting for client reqeut");
+            while (true) {
                 // listening on the port
-                Socket s = ss.accept();//establishes connection
+                Socket s = ss.accept();// establishes connection
                 System.out.println("Client connected");
                 DataInputStream dis = new DataInputStream(s.getInputStream());
 
                 String str = (String) dis.readUTF();
+                Path file = Paths.get("testfilename.xml");
+                Files.write(file, Collections.singleton(str));
                 System.out.println("message= " + str);
-                //ss.close();
-            } 
-            } catch (Exception e) {
-                System.out.println(e);
+                // ss.close();
             }
-        
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
